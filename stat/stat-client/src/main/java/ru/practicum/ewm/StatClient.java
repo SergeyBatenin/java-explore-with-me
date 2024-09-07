8a60d0452494d35ce0f26e5478f5f6c7138066f7
Sergey Batenin <inferno.87@mail.ru>
2024-09-07 21:16:40 +0300

    fix StatClient

--- stat/stat-client/src/main/java/ru/practicum/ewm/StatClient.java
+++ stat/stat-client/src/main/java/ru/practicum/ewm/StatClient.java
@@ -1,4 +1,36 @@
 package ru.practicum.ewm;
 
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.practicum.ewm.dto.ParamHitDto;
+import ru.practicum.ewm.dto.ParamStatDto;
+import ru.practicum.ewm.dto.StatInfoDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class StatClient {
+    private final WebClient client;
+
+    public StatClient(WebClient client) {
+        this.client = client;
+    }
+
+    public void save(String app, String uri, String ip, LocalDateTime timestamp) {
+        ParamHitDto payload = new ParamHitDto(app, uri, ip, timestamp);
+        client.post()
+                .uri("/hit")
+                .bodyValue(payload)
+                .retrieve()
+                .toBodilessEntity()
+                .then();
+    }
+
+    public List<StatInfoDto> getStats(ParamStatDto paramDto) {
+        return client.get()
+                .uri("/stats")
+                .retrieve()
+                .bodyToFlux(StatInfoDto.class)
+                .collectList()
+                .block();
+    }
 }
