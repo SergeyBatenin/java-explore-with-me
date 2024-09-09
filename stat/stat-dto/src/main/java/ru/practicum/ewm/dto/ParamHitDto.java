9b5aa64acdb2ea160b61fd8570b7a76d0216cf85
Sergey Batenin <inferno.87@mail.ru>
2024-09-09 20:58:13 +0300

    add validation parameters for dto based on database settings

--- stat/stat-dto/src/main/java/ru/practicum/ewm/dto/ParamHitDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/ewm/dto/ParamHitDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,13 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ParamHitDto {
     @NotBlank
+    @Size(max = 30, message = "Превышен максимальный размер названия приложения")
     private String app;
     @NotBlank
+    @Size(max = 500, message = "Превышен максимальный размер url'а")
     private String uri;
     @NotNull
+    @Size(max = 15, min = 7)
     @Pattern(regexp = "^((25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)(\\.|$)){4}$",
             message = "Некорректный формат IP-адреса")
     private String ip;
