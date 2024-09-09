ca443a5de3e65e8284743e16e2cd3118641e8f1a
Sergey Batenin <inferno.87@mail.ru>
2024-09-09 19:15:32 +0300

    remove unused code

--- stat/stat-server/src/main/java/ru/practicum/ewm/mapper/ParamMapper.java
+++ stat/stat-server/src/main/java/ru/practicum/ewm/mapper/ParamMapper.java
@@ -7,10 +7,6 @@ import ru.practicum.ewm.model.ParamHit;
 
 @Mapper(componentModel = "spring")
 public interface ParamMapper {
-//    @Mapping(target = "app", source = "paramHitDto.app")
-//    @Mapping(target = "uri", source = "paramHitDto.uri")
-//    @Mapping(target = "ip", source = "paramHitDto.ip")
-//    @Mapping(target = "timestamp", source = "paramHitDto.timestamp")
     @Mapping(target = "id", ignore = true)
     ParamHit fromDto(ParamHitDto paramHitDto);
 }
